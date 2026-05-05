85f6dd7fbbe067a7aaee87997571bd724d3b16cf
Kripa887 <55871192+Kripa887@users.noreply.github.com>
2026-05-05 23:27:05 +0530

    Update DisplayManager.cpp

--- src/DisplayManager.cpp
+++ src/DisplayManager.cpp
@@ -1,1 +1,73 @@
-h
+#include "DisplayManager.h"
+
+void DisplayManager::begin()
+{
+    lcd.init();
+
+    lcd.backlight();
+}
+
+void DisplayManager::showBoot()
+{
+    lcd.clear();
+
+    lcd.setCursor(0,0);
+    lcd.print("SoundHolics");
+
+    lcd.setCursor(0,1);
+    lcd.print("ESP12E Ready");
+}
+
+void DisplayManager::showVolume(int value)
+{
+    lcd.clear();
+
+    lcd.setCursor(0,0);
+    lcd.print("Volume:");
+    lcd.print(value);
+
+    int bars = map(value,0,48,0,16);
+
+    lcd.setCursor(0,1);
+
+    for(int i=0;i<bars;i++)
+    {
+        lcd.write(255);
+    }
+}
+
+void DisplayManager::showBass(int value)
+{
+    lcd.clear();
+
+    lcd.setCursor(0,0);
+    lcd.print("Bass:");
+    lcd.print(value);
+}
+
+void DisplayManager::showMid(int value)
+{
+    lcd.clear();
+
+    lcd.setCursor(0,0);
+    lcd.print("Mid:");
+    lcd.print(value);
+}
+
+void DisplayManager::showTreble(int value)
+{
+    lcd.clear();
+
+    lcd.setCursor(0,0);
+    lcd.print("Treble:");
+    lcd.print(value);
+}
+
+void DisplayManager::showInput(int value)
+{
+    lcd.clear();
+
+    lcd.setCursor(0,0);
+    lcd.print("Input:");
+    lcd.print(value);
+}
