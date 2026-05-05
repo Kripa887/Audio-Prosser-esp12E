798265429e0759a3b23761cd8439b278e6ad1fa9
Kripa887 <55871192+Kripa887@users.noreply.github.com>
2026-05-05 23:25:46 +0530

    Update AudioManager.h

--- src/AudioManager.h
+++ src/AudioManager.h
@@ -1,1 +1,26 @@
-b
+#pragma once
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <TDA7439.h>
+
+class AudioManager
+{
+public:
+    void begin();
+
+    void update();
+
+    void apply();
+
+    int volume = 20;
+    int bass = 0;
+    int mids = 0;
+    int treble = 0;
+    int balance = 0;
+    int input = 1;
+    int gain = 0;
+
+private:
+    TDA7439 tda;
+};
