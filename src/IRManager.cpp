8ee163a90a233f495728459034f89c41cf232dea
Kripa887 <55871192+Kripa887@users.noreply.github.com>
2026-05-05 23:28:59 +0530

    Update IRManager.cpp

--- src/IRManager.cpp
+++ src/IRManager.cpp
@@ -1,1 +1,14 @@
-g
+#include "IRManager.h"
+
+void IRManager::begin()
+{
+    irrecv.enableIRIn();
+}
+
+void IRManager::update()
+{
+    if(irrecv.decode(&results))
+    {
+        irrecv.resume();
+    }
+}
