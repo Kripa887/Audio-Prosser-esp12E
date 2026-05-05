d1461032ea3e03a672be103e42c61732992e552e
Kripa887 <55871192+Kripa887@users.noreply.github.com>
2026-05-05 23:28:36 +0530

    Update IRManager.h

--- src/IRManager.h
+++ src/IRManager.h
@@ -1,1 +1,18 @@
-h
+#pragma once
+
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+#include <IRutils.h>
+
+class IRManager
+{
+public:
+    void begin();
+
+    void update();
+
+private:
+    IRrecv irrecv = IRrecv(0);
+
+    decode_results results;
+};
