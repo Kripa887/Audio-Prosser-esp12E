6e344cb455b36903d3da7711c645a3129fa04cce
Kripa887 <55871192+Kripa887@users.noreply.github.com>
2026-05-05 23:29:32 +0530

    Update LEDManager.cpp

--- src/LEDManager.cpp
+++ src/LEDManager.cpp
@@ -1,1 +1,38 @@
-y
+#include "LEDManager.h"
+
+void LEDManager::begin()
+{
+    FastLED.addLeds<WS2812B,2,GRB>(leds,16);
+
+    FastLED.clear();
+    FastLED.show();
+}
+
+void LEDManager::wifiConnected()
+{
+    for(int i=0;i<16;i++)
+    {
+        leds[i] = CRGB::Blue;
+    }
+
+    FastLED.show();
+}
+
+void LEDManager::vuMeter(int value)
+{
+    int level = map(value,0,1024,0,16);
+
+    for(int i=0;i<16;i++)
+    {
+        if(i<level)
+        {
+            leds[i] = CRGB::Green;
+        }
+        else
+        {
+            leds[i] = CRGB::Black;
+        }
+    }
+
+    FastLED.show();
+}
