14024ea3763196f458d88b9d7a5b0621fee6b69d
Kripa887 <55871192+Kripa887@users.noreply.github.com>
2026-05-05 23:25:29 +0530

    Update Config.h

--- src/Config.h
+++ src/Config.h
@@ -1,1 +1,15 @@
-a
+#pragma once
+
+#define SDA_PIN 4
+#define SCL_PIN 5
+
+#define ENCODER_CLK 12
+#define ENCODER_DT 13
+#define ENCODER_SW 14
+
+#define IR_PIN 0
+
+#define LED_PIN 2
+#define NUM_LEDS 16
+
+#define VU_PIN A0
