b2f4d4b1ecfed5b05597f111113972eb5160fd99
Kripa887 <55871192+Kripa887@users.noreply.github.com>
2026-05-05 23:26:03 +0530

    Update AudioManager.cpp

--- src/AudioManager.cpp
+++ src/AudioManager.cpp
@@ -1,1 +1,27 @@
-v
+#include "AudioManager.h"
+
+void AudioManager::begin()
+{
+    Wire.begin();
+
+    apply();
+}
+
+void AudioManager::update()
+{
+}
+
+void AudioManager::apply()
+{
+    tda.setInput(input);
+
+    tda.inputGain(gain);
+
+    tda.setVolume(volume);
+
+    tda.setSnd(bass,1);
+    tda.setSnd(mids,2);
+    tda.setSnd(treble,3);
+
+    tda.spkAtt(4-balance,4+balance);
+}
