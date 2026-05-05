54ee249f0fe5956cd70c9a3d1a573f39b11ae548
Kripa887 <55871192+Kripa887@users.noreply.github.com>
2026-05-05 23:29:15 +0530

    Update LEDManager.h

--- src/LEDManager.h
+++ src/LEDManager.h
@@ -1,1 +1,16 @@
-u
+#pragma once
+
+#include <FastLED.h>
+
+class LEDManager
+{
+public:
+    void begin();
+
+    void wifiConnected();
+
+    void vuMeter(int value);
+
+private:
+    CRGB leds[16];
+};
