25f56032c88eb29d1b76c78ea3e76dc68238581e
Kripa887 <55871192+Kripa887@users.noreply.github.com>
2026-05-05 23:26:49 +0530

    Update DisplayManager.h

--- src/DisplayManager.h
+++ src/DisplayManager.h
@@ -1,1 +1,25 @@
-y
+#pragma once
+
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+class DisplayManager
+{
+public:
+    void begin();
+
+    void showBoot();
+
+    void showVolume(int value);
+
+    void showBass(int value);
+
+    void showMid(int value);
+
+    void showTreble(int value);
+
+    void showInput(int value);
+
+private:
+    LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27,16,2);
+};
