48d6b26be55097f1e91272960a812415e7c07e35
Kripa887 <55871192+Kripa887@users.noreply.github.com>
2026-05-05 23:28:19 +0530

    Update StorageManager.cpp

--- src/StorageManager.cpp
+++ src/StorageManager.cpp
@@ -1,1 +1,18 @@
-h
+#include "StorageManager.h"
+
+void StorageManager::begin()
+{
+    EEPROM.begin(512);
+}
+
+void StorageManager::saveVolume(int value)
+{
+    EEPROM.write(0,value);
+
+    EEPROM.commit();
+}
+
+int StorageManager::loadVolume()
+{
+    return EEPROM.read(0);
+}
