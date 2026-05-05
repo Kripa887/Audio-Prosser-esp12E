516313aca6252e545f9d60ab3fdd828e6f447f60
Kripa887 <55871192+Kripa887@users.noreply.github.com>
2026-05-05 23:27:41 +0530

    Update WebManager.cpp

--- src/WebManager.cpp
+++ src/WebManager.cpp
@@ -1,1 +1,15 @@
-h
+#include "WebManager.h"
+#include <LittleFS.h>
+
+void WebManager::begin()
+{
+    LittleFS.begin();
+
+    server.serveStatic("/",LittleFS,"/").setDefaultFile("index.html");
+
+    server.begin();
+}
+
+void WebManager::update()
+{
+}
